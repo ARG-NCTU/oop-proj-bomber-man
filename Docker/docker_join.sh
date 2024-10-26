9cfd78616d75a40edcd09e97535ed38fada4e34b
Brian2074 <brian910724@gmail.com>
2024-10-26 22:42:56 +0800

    feat(docker): add Docker setup for Bomber Man project
    - Add Dockerfile to set up the environment for the Bomber Man project
    - Include build.sh script to build and push Docker images
    - Add docker_join.sh script to join running Docker containers
    - Include docker_run.sh script to run Docker containers with necessary
      configurations for X11 forwarding and project directory mounting

---
+++ Docker/docker_join.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+BASH_OPTION=bash
+
+REPO_NAME="brian247/aoop2024"
+TAG="bomber-man"
+IMG="${REPO_NAME}:${TAG}"
+
+xhost +
+containerid=$(docker ps -aqf "ancestor=${IMG}") && echo $containerid
+docker exec -it \
+    --privileged \
+    -e DISPLAY=${DISPLAY} \
+    -e LINES="$(tput lines)" \
+    ${containerid} \
+    $BASH_OPTION
+xhost -
